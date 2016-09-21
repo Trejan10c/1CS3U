b1dfc16687544c7ac070535493802f35fd34f074
323629410 <323629410@C-JWZ7KS1.tdsb.on.ca>
2016-09-21 12:44:40 -0400

    About me part 1

---
+++ Hello/src/greaves/AboutMe.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package greaves;
+
+/**
+ * ICS3U
+ * AboutMe
+ * 14/09/2016
+ * Trejan Greaves
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.format("%-10s","Trejan.G\n");
+	System.out.format("%10s","Miss.Kemp\n");
+	System.out.format("%10s","Runnymede Collegiate Insitute\n");
+	System.out.format("%10s","\"Go Ravens!\"\n\n");
+	}
+
+}
